0167bfdfcec40860860ff4dac1e41b492e2516ca
Shruti Agrawal <shruti98932@gmail.com>
2021-07-10 19:40:47 -0400

    Implemented async-word-count

--- async-word-count/README.md
+++ async-word-count/README.md
@@ -2,8 +2,8 @@
 
 Inside `async-word-count.js`, complete `getTotalWordCount`. `getTotalWordCount` should pass the combined word count of the files located at `filePathOne` and `filePathTwo` to the `callback` following proper node style convention
 
-- [ ] Be sure to handle errors at each step
-- [ ] Do not use promises
+- [X] Be sure to handle errors at each step
+- [X] Do not use promises
 
 ### Available Resources for this Prompt
 
--- async-word-count/async-word-count.js
+++ async-word-count/async-word-count.js
@@ -13,8 +13,22 @@ var getWordCount = function(filePath, callback) {
   });
 };
 
-var getTotalWordCount = function(filePathOne, filePathTwo, callback) {
+var getTotalWordCount = function('./fileOne.txt', './fileTwo.txt', callback) {
   // YOUR CODE HERE
+  getWordCount('./fileOne.txt', (err, count1) => {
+    if(err) {
+      console.log('Error caught while reading File One');
+    } else {
+      getWordCount('./fileTwo.txt', (err, count2) => {
+        if(err) {
+          console.log('Error caught while reading File Two')''
+        } else {
+          var totalCount = count1 + count2;
+          callback(totalCount)
+        }
+      })
+    }
+  })
 };
 
 module.exports = getTotalWordCount;
